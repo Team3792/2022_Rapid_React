cc0feab8f19f25715834036ed3e92bca87e5af7e
zhouzih04 <46199489+zhouzih04@users.noreply.github.com>
2022-02-26 00:41:41 -0600

    fix some funnies

--- src/main/java/com/AA3792/CustomButtons/RStickRight.java
+++ src/main/java/com/AA3792/CustomButtons/RStickRight.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package com.AA3792.Joystick;
+package com.AA3792.CustomButtons;
 
 // import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
